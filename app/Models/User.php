2b1a26823038690c2a06f727194c87351ba82058
DMM-IT <macaraigdrew99@gmail.com>
2026-07-22 18:32:31 +0800

    Add api_token to User fillable attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,7 +15,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'is_staff', 'is_admin', 'admin_permissions'])]
+#[Fillable(['name', 'email', 'password', 'is_staff', 'is_admin', 'admin_permissions', 'api_token'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements FilamentUser
 {
