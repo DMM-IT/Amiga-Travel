25985e9707da36fa511518f6b5ca52bebb3991bc
DMM-IT <macaraigdrew99@gmail.com>
2026-07-23 16:12:02 +0800

    .

--- resources/views/livewire/booking-form.blade.php
+++ resources/views/livewire/booking-form.blade.php
@@ -511,9 +511,6 @@
                                         @endphp
 
                                         @php
-    $selectedSchedule = collect($availableSchedules)->firstWhere('id', $selected_schedule_id);
-@endphp
-<pre style="background:#000;color:#0f0;padding:1rem;font-size:11px;overflow:auto;max-height:400px;">{{ json_encode(['mode'=>$mode,'selected_schedule_id'=>$selected_schedule_id,'available_ids'=>collect($availableSchedules)->pluck('id'),'selectedSchedule'=>$selectedSchedule], JSON_PRETTY_PRINT) }}</pre>
 
                                         {{-- Ferry: Show accommodations --}}
                                         @if($mode === 'ferry' && $selectedSchedule && !empty($selectedSchedule['accommodations']))
