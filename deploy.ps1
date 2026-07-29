8ce6ea1a3ab52669f4f96dcc08494f18c297666b
DMM-IT <macaraigdrew99@gmail.com>
2026-07-30 03:14:27 +0800

    Fix APK output filename in deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -6,7 +6,7 @@ Write-Host 'Building Web...'
 flutter build web --release
 cd ..
 Write-Host 'Copying files...'
-Copy-Item -Path flutter_app\build\app\outputs\flutter-apk\app-release.apk -Destination public\downloads\amiga-gracia.apk -Force
+Copy-Item -Path flutter_app\build\app\outputs\flutter-apk\app-release.apk -Destination public\downloads\amiga-travel.apk -Force
 Copy-Item -Path flutter_app\build\web\* -Destination public\app\ -Recurse -Force
 Write-Host 'Deployment finished.'
 
